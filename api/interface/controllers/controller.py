f5087065f8b8e6f2e86ab12e60684410574a0fa8
Do-yoon <1.kim.doyoon@gmail.com>
2024-10-31 11:49:30 +0900

    DK-102 controller에 service 추가

--- api/interface/controllers/controller.py
+++ api/interface/controllers/controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from api.application.service import Service
 from dependency_injector.wiring import inject, Provide
+from containers import Container
 
 router = APIRouter(prefix='/ai')
 
@@ -26,4 +27,11 @@ def daily(
         req: DailyRequestDTO,
         service: Service = Depends(Provide[Container.service])
 ) -> DailyResponseDTO:
-    pass
+    missions = service(req.user_id, req.fields)
+    res = [
+        Mission(level="상", content=missions[0]),
+        Mission(level="중", content=missions[1]),
+        Mission(level="하", content=missions[2])
+    ]
+
+    return DailyResponseDTO(missions=res)
